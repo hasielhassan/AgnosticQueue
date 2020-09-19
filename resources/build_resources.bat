b732fa42469ff3b35a3cd29c16b4426ef7a81135
Hasiel Alvarez <hasielhassan@gmail.com>
2020-09-18 22:47:41 -0700

    update screenshot, removed test file in favor of sample one and update the build resources script

--- resources/build_resources.bat
+++ resources/build_resources.bat
@@ -1,3 +1,10 @@
-pyside-uic --from-imports job_card_form.ui > ../python/ui/job_card_form.py
-pyside-uic --from-imports queue_dialog_form.ui> ../python/ui/queue_dialog_form.py
-pyside-uic --from-imports queue_overview_form.ui> ../python/ui/queue_overview_form.py
+:: be sure to have the proper pyside2 uic on path
+pyside2-uic --from-imports job_card_form.ui > ../python/ui/job_card_form.py
+pyside2-uic --from-imports queue_dialog_form.ui> ../python/ui/queue_dialog_form.py
+pyside2-uic --from-imports queue_overview_form.ui> ../python/ui/queue_overview_form.py
+
+:: you actually need to replace this paths with fullpaths
+:: Qt --convert doesn't support relative paths
+python -m Qt --convert ../python/ui/job_card_form.py
+python -m Qt --convert ../python/ui/queue_dialog_form.py
+python -m Qt --convert ../python/ui/queue_overview_form.py
